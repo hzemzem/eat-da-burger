d629c9b909770ca14bd082f29e8c5836ea045240
anas tellat <anas tellat>
2017-08-15 16:50:26 -0400

    deleted old repo and created a new one

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Falafel Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', true);
